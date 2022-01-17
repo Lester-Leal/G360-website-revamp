6f59df8a47c2ecad6ed3f9d2d5cbeb4222609811
Alfon Labadan <47669262+codepurse@users.noreply.github.com>
2022-01-17 23:48:01 +0800

    a

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/layouts/layout";
-import "../styles/_globals.scss";
+import "../styles/globals.scss";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 function MyApp({ Component, pageProps }) {
