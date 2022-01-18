f36f8846f8207ea8d0ef40a4c3e22b8f0c618dc4
Alfon Labadan <47669262+codepurse@users.noreply.github.com>
2022-01-18 23:53:55 +0800

    Update layout.js

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -31,6 +31,10 @@ const Layout = ({ children }) => {
           href="https://fonts.googleapis.com/css2?family=Vollkorn:wght@400;500;700;800&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <Script src="/script/3dGlobe.js" />
       <div
