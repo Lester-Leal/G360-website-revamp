16cf2f7a591829abf7012f7e359531a2a50d0481
Alfon Labadan <47669262+codepurse@users.noreply.github.com>
2022-01-18 23:47:55 +0800

    Update font

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -28,7 +28,7 @@ const Layout = ({ children }) => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Vollkorn:wght@500;600;700;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Vollkorn:wght@400;500;700;800&display=swap"
           rel="stylesheet"
         />
       </Head>
