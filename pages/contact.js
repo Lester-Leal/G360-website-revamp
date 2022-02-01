233c409de708badf1bf469f15d6c216bbe84da26
Alfon Labadan <47669262+codepurse@users.noreply.github.com>
2022-02-02 00:36:06 +0800

    t

--- pages/contact.js
+++ pages/contact.js
@@ -1,27 +1,23 @@
 import { Container, Row, Col } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
-const styles = (theme) => ({
-  textField: {
-    width: "90%",
-    marginLeft: "auto",
-    marginRight: "auto",
-    paddingBottom: 0,
-    marginTop: 0,
-    fontWeight: 500,
-  },
-  input: {
-    color: "white",
-  },
-});
+
 export default function contact() {
-  const classes = styles;
   return (
     <Container fluid className="h-100 conContact">
-      <Row className="h-100 align-items-center justify-content-center d-flex">
-        <Col lg={6}></Col>
-        <Col lg={6}></Col>
-      </Row>
+      <div className="divImg"></div>
+      <Container className="h-100">
+        <Row className="h-100 align-items-center justify-content-center d-flex">
+          <Col lg={6}>
+            <p>We Copde</p>
+          </Col>
+          <Col lg={6}>
+            <div className="divRight">
+              <p>We Copde</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 }
