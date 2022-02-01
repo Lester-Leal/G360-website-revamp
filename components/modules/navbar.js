234194ece1e69f1f45b8ffde796b7d63786a5e84
Alfon Labadan <47669262+codepurse@users.noreply.github.com>
2022-02-01 14:49:19 +0800

    t

--- components/modules/navbar.js
+++ components/modules/navbar.js
@@ -8,6 +8,7 @@ export default function navbar(props) {
   const [scrollNav, setScrollnav] = useState(false);
   const [full, setFull] = useState(false);
   const [pathUrl, setPathUrl] = useState("");
+  const [checked, setChecked] = useState(false);
   const urlPath = router.pathname;
   const changeBackground = () => {
     if (window.scrollY >= 70) {
@@ -31,118 +32,81 @@ export default function navbar(props) {
     }
   }, [full]);
 
-
   return (
     <>
       <Container fluid className="navFull">
         <Container className="h-100">
           <Row className="align-items-center justify-content-center d-flex h-100">
-            <Col lg={12}>
-              <div className="mx-auto">
-                <Row>
-                  <Col lg={4}>
-                    <p className="pHeader">FEATURED WORK</p>
-                    <div className="divClient">
-                      <p className="pClient">WASHWELL</p>
-                      <p className="pDesc">
-                        They work to provide a simple high-quality solution to
-                        take care of everything in your closet so you have the
-                        time for what really matters.
-                      </p>
-                    </div>
-                    <div className="divClient">
-                      <p className="pClient">Enderun</p>
-                      <p className="pDesc">
-                        A private non-sectarian undergraduate college situated
-                        at the Bonifacio Global City in Taguig, Metro Manila,
-                        Philippines
-                      </p>
-                    </div>
-                    <div className="divClient">
-                      <p className="pClient">Lifeline</p>
-                      <p className="pDesc">
-                        We provide 24/7 quick & reliable response and
-                        transportation while providing the finest medical care
-                        when transporting patients. "Virtual Emergency Room On
-                        Wheels".
-                      </p>
-                    </div>
-                    <div className="divClient">
-                      <p className="pClient">Phoenix</p>
-                      <p className="pDesc">
-                        Is the first independent oil company to be listed in the
-                        Philippine Stock Exchange after the Oil Deregulation Law
-                        was passed in 1998
-                      </p>
-                    </div>
-                  </Col>
-                  <Col lg={4}>
-                    <div className="mx-auto d-block" style={{ width: "200px" }}>
-                      <p className="pHeader">LINKS</p>
-                      <div className="divClient">
-                        <p className="pMenu">Home</p>
-                      </div>
-                      <div className="divClient">
-                        <p className="pMenu">Services</p>
-                      </div>
-                      <div className="divClient">
-                        <a href="/about">
-                          <p className="pMenu">About</p>
-                        </a>
-                      </div>
-                      <div className="divClient">
-                        <p className="pMenu">Portfolio</p>
-                      </div>
-                      <div className="divClient">
-                        <p className="pMenu">Message Us</p>
-                      </div>
-                    </div>
-                  </Col>
-                  <Col lg={4}>
-                    <p className="pHeader">Contact Info</p>
-                    <div className="divClient">
-                      <p className="pMenu1">Location | PH</p>
-                      <p className="pDesc" style={{ maxWidth: "300px" }}>
-                        290 Aguirre Ave, Parañaque, 1720 Metro Manila
-                      </p>
-                    </div>
-                    <div className="divClient">
-                      <p className="pMenu1">Phone Number</p>
-                      <p className="pDesc">
-                        +63 917 766 1380 | +63 999 124 4211
-                      </p>
-                    </div>
-                    <div className="divClient">
-                      <p className="pMenu1">Email</p>
-                      <p className="pDesc">info@guerilla360.com</p>
-                    </div>
-                    <div className="divClient">
-                      <p className="pMenu1">Socials</p>
-                      <ul>
-                        <li>
-                          <a href="#">
-                            <i className="fab fa-facebook-f icon"></i>{" "}
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="fab fa-twitter icon"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="fab fa-linkedin-in icon"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="fab fa-google-plus-g icon"></i>
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </Col>
-                </Row>
+            <Col lg={8}>
+              <div className="form-inline">
+                <p className="pCount">01</p>
+                <p className="pMenu Animation">Services</p>
+              </div>
+              <div className="form-inline">
+                <p className="pCount">02</p>
+                <p className="pMenu Animation">Portfolio</p>
+              </div>
+              <div className="form-inline">
+                <p className="pCount">03</p>
+                <p
+                  className="pMenu Animation"
+                  onClick={(e) => {
+                    router.push("/about");
+                    setFull(false);
+                    document
+                      .querySelector(".navFull")
+                      .classList.toggle("showNav");
+                    setChecked((full) => !full);
+                  }}
+                >
+                  About
+                </p>
+              </div>
+              <div className="form-inline">
+                <p className="pCount">04</p>
+                <p className="pMenu Animation">Careers</p>
+              </div>
+            </Col>
+            <Col lg={4}>
+              <p className="pHeader">Contact Info</p>
+              <div className="divClient">
+                <p className="pMenu1">Location | PH</p>
+                <p className="pDesc" style={{ maxWidth: "300px" }}>
+                  290 Aguirre Ave, Parañaque, 1720 Metro Manila
+                </p>
+              </div>
+              <div className="divClient">
+                <p className="pMenu1">Phone Number</p>
+                <p className="pDesc">+63 917 766 1380 | +63 999 124 4211</p>
+              </div>
+              <div className="divClient">
+                <p className="pMenu1">Email</p>
+                <p className="pDesc">info@guerilla360.com</p>
+              </div>
+              <div className="divClient">
+                <p className="pMenu1">Socials</p>
+                <ul>
+                  <li>
+                    <a href="#">
+                      <i className="fab fa-facebook-f icon"></i>{" "}
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fab fa-twitter icon"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fab fa-linkedin-in icon"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fab fa-google-plus-g icon"></i>
+                    </a>
+                  </li>
+                </ul>
               </div>
             </Col>
           </Row>
@@ -151,30 +115,38 @@ export default function navbar(props) {
       <Container
         fluid
         className="divNavbar"
-        style={{ background: full || urlPath === "/contact" ? "transparent" : "" }}
+        style={{
+          background: full || urlPath === "/contact" ? "transparent" : "",
+        }}
       >
         <Row>
           <Col lg={6}>
             <img
-              src={!full ? "Image/logo_white.png" : "Image/logo_dark.png"}
+              src={!full ? "Image/logo_white.png" : ""}
               className="img-fluid"
               style={{ width: "140px" }}
             />
           </Col>
           <Col lg={6}>
-            <div style={{ position: "relative" }}>
-              <div
-                className={full ? "toggle-icon pushed" : "toggle-icon"}
-                onClick={(e) => {
-                  setFull((full) => !full);
-                  document
-                    .querySelector(".navFull")
-                    .classList.toggle("showNav");
-                }}
-              >
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
+            <div style={{ postiion: "relative" }}>
+              <div id="webapp_cover">
+                <div id="menu_button">
+                  <input
+                    type="checkbox"
+                    id="menu_checkbox"
+                    checked={checked}
+                    onChange={(e) => {
+                      setFull((full) => !full);
+                      setChecked((full) => !full);
+                      document
+                        .querySelector(".navFull")
+                        .classList.toggle("showNav");
+                    }}
+                  />
+                  <label htmlFor="menu_checkbox" id="menu_label">
+                    <div id="menu_text_bar" />
+                  </label>
+                </div>
               </div>
             </div>
           </Col>
--- components/modules/navbarbackup.js
+++ components/modules/navbarbackup.js
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { useRouter } from "next/router";
-export default function navbar() {
+export default function navbar(props) {
   const router = useRouter();
   const [scrollNav, setScrollnav] = useState(false);
+  const [full, setFull] = useState(false);
   const [pathUrl, setPathUrl] = useState("");
   const urlPath = router.pathname;
   const changeBackground = () => {
@@ -23,77 +24,162 @@ export default function navbar() {
     window.addEventListener("scroll", changeBackground, true);
   });
   useEffect(() => {
-    setPathUrl(router.path);
-  }, [urlPath]);
+    if (full) {
+      props.show();
+    } else {
+      props.unShow();
+    }
+  }, [full]);
+
 
   return (
-    <Navbar
-      className={
-        !scrollNav
-          ? "navbarBg navbarTransparent justify-content-between"
-          : "navbarBg navbarTransparent justify-content-between"
-      }
-      id="navbarBg"
-      fixed="top"
-    >
-      <Container fluid id={!scrollNav ? "conNav" : "conNavNoPad"}>
-        <Nav className="ml-auto">
-          <Navbar.Brand href="/">
+    <>
+      <Container fluid className="navFull">
+        <Container className="h-100">
+          <Row className="align-items-center justify-content-center d-flex h-100">
+            <Col lg={12}>
+              <div className="mx-auto">
+                <Row>
+                  <Col lg={4}>
+                    <p className="pHeader">FEATURED WORK</p>
+                    <div className="divClient">
+                      <p className="pClient">WASHWELL</p>
+                      <p className="pDesc">
+                        They work to provide a simple high-quality solution to
+                        take care of everything in your closet so you have the
+                        time for what really matters.
+                      </p>
+                    </div>
+                    <div className="divClient">
+                      <p className="pClient">Enderun</p>
+                      <p className="pDesc">
+                        A private non-sectarian undergraduate college situated
+                        at the Bonifacio Global City in Taguig, Metro Manila,
+                        Philippines
+                      </p>
+                    </div>
+                    <div className="divClient">
+                      <p className="pClient">Lifeline</p>
+                      <p className="pDesc">
+                        We provide 24/7 quick & reliable response and
+                        transportation while providing the finest medical care
+                        when transporting patients. "Virtual Emergency Room On
+                        Wheels".
+                      </p>
+                    </div>
+                    <div className="divClient">
+                      <p className="pClient">Phoenix</p>
+                      <p className="pDesc">
+                        Is the first independent oil company to be listed in the
+                        Philippine Stock Exchange after the Oil Deregulation Law
+                        was passed in 1998
+                      </p>
+                    </div>
+                  </Col>
+                  <Col lg={4}>
+                    <div className="mx-auto d-block" style={{ width: "200px" }}>
+                      <p className="pHeader">LINKS</p>
+                      <div className="divClient">
+                        <p className="pMenu">Home</p>
+                      </div>
+                      <div className="divClient">
+                        <p className="pMenu">Services</p>
+                      </div>
+                      <div className="divClient">
+                        <a href="/about">
+                          <p className="pMenu">About</p>
+                        </a>
+                      </div>
+                      <div className="divClient">
+                        <p className="pMenu">Portfolio</p>
+                      </div>
+                      <div className="divClient">
+                        <p className="pMenu">Message Us</p>
+                      </div>
+                    </div>
+                  </Col>
+                  <Col lg={4}>
+                    <p className="pHeader">Contact Info</p>
+                    <div className="divClient">
+                      <p className="pMenu1">Location | PH</p>
+                      <p className="pDesc" style={{ maxWidth: "300px" }}>
+                        290 Aguirre Ave, Parañaque, 1720 Metro Manila
+                      </p>
+                    </div>
+                    <div className="divClient">
+                      <p className="pMenu1">Phone Number</p>
+                      <p className="pDesc">
+                        +63 917 766 1380 | +63 999 124 4211
+                      </p>
+                    </div>
+                    <div className="divClient">
+                      <p className="pMenu1">Email</p>
+                      <p className="pDesc">info@guerilla360.com</p>
+                    </div>
+                    <div className="divClient">
+                      <p className="pMenu1">Socials</p>
+                      <ul>
+                        <li>
+                          <a href="#">
+                            <i className="fab fa-facebook-f icon"></i>{" "}
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">
+                            <i className="fab fa-twitter icon"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">
+                            <i className="fab fa-linkedin-in icon"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">
+                            <i className="fab fa-google-plus-g icon"></i>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Container
+        fluid
+        className="divNavbar"
+        style={{ background: full || urlPath === "/contact" ? "transparent" : "" }}
+      >
+        <Row>
+          <Col lg={6}>
             <img
-              src={!scrollNav ? "Image/logo_white.png" : "Image/logo_white.png"}
+              src={!full ? "Image/logo_white.png" : "Image/logo_dark.png"}
               className="img-fluid"
               style={{ width: "140px" }}
             />
-          </Navbar.Brand>
-        </Nav>
-        <Nav
-          className="mx-auto text-center"
-          style={{
-            position: "absolute",
-            left: "50%",
-            transform: "translate(-50%)",
-          }}
-        >
-          <Nav.Link
-            href="#home"
-            className={scrollNav ? "navLight" : "navLight"}
-            id={urlPath === "/" ? "activeNav" : ""}
-          >
-            Home
-          </Nav.Link>
-          <Nav.Link
-            href="about"
-            className={scrollNav ? "navLight" : "navLight"}
-          >
-            About
-          </Nav.Link>
-          <Nav.Link
-            href="#pricing"
-            className={scrollNav ? "navLight" : "navLight"}
-          >
-            Services
-          </Nav.Link>
-
-          <Nav.Link
-            href="#pricing"
-            className={scrollNav ? "navLight" : "navLight"}
-            style={{ marginRight: "15px" }}
-          >
-            Careers
-          </Nav.Link>
-        </Nav>
-        <Nav className="ms-auto">
-          <Navbar.Brand href="/">
-            <Nav.Link
-              href="#pricing"
-              id="navContact"
-              className={scrollNav ? "navLight" : "navLight"}
-            >
-              Contact Us
-            </Nav.Link>
-          </Navbar.Brand>
-        </Nav>
+          </Col>
+          <Col lg={6}>
+            <div style={{ position: "relative" }}>
+              <div
+                className={full ? "toggle-icon pushed" : "toggle-icon"}
+                onClick={(e) => {
+                  setFull((full) => !full);
+                  document
+                    .querySelector(".navFull")
+                    .classList.toggle("showNav");
+                }}
+              >
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </div>
+            </div>
+          </Col>
+        </Row>
       </Container>
-    </Navbar>
+    </>
   );
 }
