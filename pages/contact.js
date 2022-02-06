2a4580def0d576cf3507f5344d0635c6345c0cca
Alfon Labadan <47669262+codepurse@users.noreply.github.com>
2022-02-07 00:04:14 +0800

    tedt

--- pages/contact.js
+++ pages/contact.js
@@ -29,7 +29,60 @@ export default function contact() {
                 No time to chat at the moment? Use our form instead.
               </p>
               <div className="divInnerForm">
-                <p className="pForm">Hi Guerilla360,</p>
+                <p className="pGreetings">Hi Guerilla360,</p>
+                <div className="form-inline">
+                  <p className="pForm">My name is,</p>
+                  <div className="input-box">
+                    <input type="text" id="field" placeholder=" " required />
+                    <label className="lbl">First name</label>
+                  </div>
+                  <div className="input-box">
+                    <input type="text" id="field" placeholder=" " required />
+                    <label className="lbl">Last name</label>
+                  </div>
+                </div>
+                <div className="form-inline">
+                  <p className="pForm">and I work with</p>
+                  <div className="input-box">
+                    <input type="text" id="field" placeholder=" " required />
+                    <label className="lbl">Company name</label>
+                  </div>
+                  <p className="pForm">as</p>
+                  <div className="input-box">
+                    <input type="text" id="field" placeholder=" " required />
+                    <label className="lbl">Position</label>
+                  </div>
+                </div>
+                <div className="form-inline">
+                  <p className="pForm">I would love to talk about</p>
+                  <div className="input-box">
+                    <input type="text" id="field" placeholder=" " required />
+                    <label className="lbl">Your enquiry</label>
+                  </div>
+                </div>
+                <div className="form-inline">
+                  <p className="pForm">You can reach me at</p>
+                  <div className="input-box">
+                    <input type="text" id="field" placeholder=" " required />
+                    <label className="lbl">Email address</label>
+                  </div>
+                </div>
+                <div>
+                  <input
+                    className="inp-cbx"
+                    id="cbx"
+                    type="checkbox"
+                    style={{ display: "none" }}
+                  />
+                  <label className="cbx" htmlFor="cbx">
+                    <span>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span>
+                    <span className = "spanText">I agree my information can be used to contact me regarding my enquiry.</span>
+                  </label>
+                </div>
               </div>
             </div>
           </Col>
